eb300c18b80dc952d00a10530975ebf535bb2a55
Joao Ferreira (SAO) <Joao.Ferreira2@fit-tecnologia.org.br>
2025-02-26 14:16:12 -0300

    add the words list

---
+++ public/data/words.js
@@ -0,0 +1,38 @@
+export const words = [
+  {
+    id: 1,
+    name: "Fusca",
+    clue: "Carro antigo desenvolvido nos anos 30",
+  },
+
+  {
+    id: 2,
+    name: "Ronaldinho Gaucho",
+    clue: "Jogador de futebol com grande destaque por sua habilidade",
+  },
+  {
+    id: 3,
+    name: "Coca Cola",
+    clue: "Marca de refrigerantes conhecida mundialmente",
+  },
+  {
+    id: 4,
+    name: "Bic",
+    clue: "Empresa conhecida por fabricar e vender produtos nas mais diversas áreas",
+  },
+  {
+    id: 5,
+    name: "Geladeira",
+    clue: "Eletrodoméstico para uso caseiro que se tornou primordial da metade do século XX em diante",
+  },
+  {
+    id: 6,
+    name: "Lapis",
+    clue: "Item que pode ser empregado em diversos ambientes, desde o escolar até o corporativo",
+  },
+  {
+    id: 7,
+    name: "Manaus",
+    clue: "Cidade conhecida mundialmente pela sua relevância ambiental",
+  },
+];
